1b04583393c19099ab260892908c36617cefa217
XvarX <yexangjun@qq.com>
2026-01-07 22:08:15 +0800

    lesson1 finish 完成通过find_package调用安装到系统的库

--- lesson2/src/main.cpp
+++ lesson2/src/main.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<mymath/mymath.h>
+#include<mymath/arithmetic.h>
 
 using namespace std;
 
 int main() {
     cout << "hello world" << endl;
-    cout << "1 + 2 = " << MyMath::add(1, 2) << endl;
+    cout << "1 + 2 = " << CArithmetic::add(1, 2) << endl;
 
     return 0;
 }
